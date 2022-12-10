0c182b3a1c3393fcdf6ea29df490bd217f74dca8
gatssp <110286579+gatssp@users.noreply.github.com>
2022-12-10 23:35:17 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
     public double calculate(int credit, int time, double percent) {
-        double creditRate = percent / 12 / 100;     // ставка в месяц
-        double degree = Math.pow(1 + x, time); // степень
-        double annuity = creditRate * degree / (degree-1);        // аннуитет
+        double creditRate = percent / 12 / 100;              // ставка в месяц
+        double degree = Math.pow(1 + x, time);              // стерень 
+        double annuity = creditRate * degree / (degree-1); // аннуитет
         double payment = annuity * credit;
         return payment;
     }
