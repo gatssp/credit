67baf19ced533ab66d93bd0ad99c89df06fed1d3
gatssp <110286579+gatssp@users.noreply.github.com>
2022-12-10 23:33:03 +0300

    Add percent in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double payment = service.calculate( 1_000_000, 12);
+        double payment = service.calculate( 1_000_000, 12, 9.99);
 
         System.out.println(payment);
     }
