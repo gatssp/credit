0500055d0de05c0ba1ca69ce3d2956f8e4300de5
Polina Shebysheva <gatsbysober@gmail.com>
2022-12-12 21:27:56 +0300

    edit names

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public double calculate(int credit, int time, double percent) {
         double creditRate = percent / 12 / 100;              // ставка в месяц
-        double degree = Math.pow(1 + x, time);              // стерень 
+        double degree = Math.pow(1 + creditRate, time);              // стерень
         double annuity = creditRate * degree / (degree-1); // аннуитет
         double payment = annuity * credit;
         return payment;
